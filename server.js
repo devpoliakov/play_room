32567d137a101b52b2f185c9924ae1746863a0dd
Yurii Poliakov <kontynent@ua.fm>
2021-03-15 09:34:06 +0200

    Basic socket code

--- server.js
+++ server.js
@@ -14,3 +14,11 @@ app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
 });
 
+io.sockets.on('connection', function(socket){
+	connections.push(socket);
+	console.log('Connected: %s sockets connected', connections.length);
+
+	// disconnect
+	connections.splice(connections.indexOf(socket), 1);
+	console.log('Disconnected: %s sockets connected', connections.length);
+});
