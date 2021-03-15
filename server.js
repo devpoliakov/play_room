b93912d2f7ace4dfb904788b103e52d38ba67978
Yurii Poliakov <kontynent@ua.fm>
2021-03-15 12:27:52 +0200

    Simple socket connection

--- server.js
+++ server.js
@@ -21,7 +21,8 @@ io.sockets.on('connection', function(socket){
 	connections.push(socket);
 	console.log('Connected: %s sockets connected', connections.length);
 
-	// disconnect
-	connections.splice(connections.indexOf(socket), 1);
-	console.log('Disconnected: %s sockets connected', connections.length);
+	socket.on('disconnect', function(data){
+		connections.splice(connections.indexOf(socket), 1);
+		console.log('Disconnected: %s sockets connected', connections.length);
+	});
 });
